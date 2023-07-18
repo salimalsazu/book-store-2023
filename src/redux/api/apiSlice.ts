c7ad6e9e5e8cc37e69392d5ec6ebfac16f012f3b
Salim Al Sazu <accsalimalsazu@gmail.com>
2023-07-18 23:46:06 +0600

    bugs fixed

--- src/redux/api/apiSlice.ts
+++ src/redux/api/apiSlice.ts
@@ -4,8 +4,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const api = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    // baseUrl: "https://book-store-server-weld.vercel.app/api/v1",
-    baseUrl: "http://localhost:7000/api/v1",
+    baseUrl: "https://book-store-server-weld.vercel.app/api/v1",
   }),
   endpoints: () => ({}),
   tagTypes: ["books", "wish", "read"],
